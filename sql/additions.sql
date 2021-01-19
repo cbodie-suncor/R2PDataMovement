e268e4a29ea006dab13cb22cd9901d5216572def
cbodie-suncor <cbodie@suncor.com>
2021-01-18 22:08:39 -0700

    initial commit

---
+++ sql/additions.sql
@@ -0,0 +1,31 @@
+
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('BSUNCOR','CP04','PRODMP02','10117','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('BSUNOSP','CP04','PRODMP02','10119','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('BSYNCR','CP04','PRODMP02','10121','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('D50','CP04','PRODMP02','10123','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('HSR','CP04','PRODMP02','10125','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('JETA','CP04','PRODMP02','10127','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('STOM','CP04','PRODMP02','10128','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('FCOMFG','CP04','PRODMP02','10129','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('FNGAS','CP04','PRODMP02','10130','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('PEFLGNF','CP04','PRODMP02','10132','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('C5C6','CP04','PRODMP02','10133','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('DILUENT','CP04','PRODMP02','10136','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('RBOB','CP04','PRODMP02','10134','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('RGUA','CP04','PRODMP02','10135','SUNCOR','L15');
+
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('14BUT_CONS','CP01','PRODMP01','10117','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('14GZ_COMB_RAFFR','CP01','PRODMP01','10119','','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('14GZ_REJ_CONS','CP01','PRODMP01','10121','SUNCORTN','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('14PROP_CONS','CP01','PRODMP01','10123','','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('48AMNIAC_CONS','CP01','PRODMP01','10125','SHELL','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('14GAZ_NAT','CP01','PRODMP01','10127','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('02COKE_CONS','CP01','PRODMP01','10128','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('14MAZ_RAFF_CONS','CP01','PRODMP01','10129','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('26GZ_TRCH_CONS','CP01','PRODMP01','10130','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('BRUT_AMNA','CP01','PRODMP01','10132','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('BRUT_ASGARD','CP01','PRODMP01','10133','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('BRUT_AZERI','CP01','PRODMP01','10136','SUNCOR','L15');
+insert into tagmap(Tag, Plant, WorkCenter, MaterialNumber, DefaultValuationType, DefaultUnit)values('BRUT_BAKKEN_NDS_RAIL','CP01','PRODMP01','10134','SUNCOR','L15');
+
+alter table batch add filename varchar(200)
